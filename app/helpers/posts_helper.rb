8202402fafb6109379c8f19033e8f6b188d45b16
masaya7 <52489219+masaya7@users.noreply.github.com>
2023-08-05 12:09:16 +0900

    fix:empathyボタンをDRYにする

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+  def render_empathy_icon(emotion, post_id)
+    case emotion
+    when :happy
+      '😀'
+    when :sad
+      '😢'
+    when :love
+      '😍'
+    when :anger
+      '😤'
+    else
+      ''
+    end
+  end
 end
