834b145674cc7e468b277368e0743600a3af3129
masaya7 <52489219+masaya7@users.noreply.github.com>
2023-08-09 17:06:40 +0900

    add:indexのcss

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,4 +3,6 @@
 pin "application", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
+pin "jquery" # @3.7.0
+
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
